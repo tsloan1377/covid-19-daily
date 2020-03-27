e6476dd7246616183c4bd06e366831802d854019
Tyler Sloan <quorumetrix@gmail.com>
2020-03-27 08:01:42 -0400

    update url
    The URL changed

--- covid.py
+++ covid.py
@@ -8,7 +8,8 @@ import plotly
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
-url_confirmed = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv'
+#url_confirmed = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv'
+url_confirmed = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
 
 class Covid:
 
